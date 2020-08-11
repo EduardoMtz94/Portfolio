c43cdc8ec8c3cb127bcda16c33875b09284573fc
Eduardo Martinez <eduardo.martinez.sanchez@hotmail.com>
2020-08-11 00:17:25 -0500

    creating about, skills, contact and footer

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -9,19 +9,70 @@
                 </ul>
             </div>
             <nav class="menu">
-                <a href="#" id="about">Sobre mi</a>
+                <a href="#about" id="about">Sobre mi</a>
                 <a href="#" id="portfolio">Portafolio</a>
-                <a href="#" id="skills">Habilidades</a>
-                <a href="#" id="contact">Contacto</a>
+                <a href="#" id="contact">Contactame</a>
             </nav>
     
             <div class="title">
                 <h1>
-                    <span>Que onda,</span><br>
+                    <span>Hola,</span><br>
                     <span>Soy Eduardo Mart&iacute;nez,</span><br> 
                     <span>Un Desarrollador Web Intergal&aacute;ctico</span>
                 </h1>
             </div>
         </div>
     </header>
+    <section class="main">
+        <section class="about" id="about">
+            <div class="content">
+                <div class="foto">
+                    <img src="/img/me.jpeg" alt="Eduardo Martinez">
+                </div>
+                <article>
+                    <h2>Sobre m&iacute;</h2>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab cupiditate dolorem debitis soluta voluptates in aliquam nemo exercitationem fugiat quia dolor tempora placeat dolores temporibus inventore consequuntur amet, ad necessitatibus.</p>
+                </article>
+            </div>
+        </section>
+        <section class="skills">
+            <div class="content">
+                <h2>Mis habilidades</h2>
+                <ul>
+                    <li>PHP</li>
+                    <li>Laravel</li>
+                    <li>Javascript</li>
+                    <li>Python</li>
+                    <li>MySQL</li>
+                    <li>Git</li>
+                    <li>SASS</li>
+                    <li>Linux</li>
+                    <li>HTML & CSS</li>
+                </ul>
+                <div class="about-skills">
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae, officia amet provident nostrum sed assumenda harum modi enim autem ducimus quibusdam optio nesciunt laudantium, deleniti quod quae ut, deserunt dolor.</p>
+                </div>
+            </div>
+        </section>
+        <section class="portfolio" id="portfolio">
+            <div class="content"></div>
+        </section>
+        <section class="contact" id="contact">
+            <div class="content">
+                <h2>Cont&aacute;ctame</h2>
+                <p>
+                    <a href="mailto:eduardo.martinez.sanchez@hotmail.com">
+                        <b>eduardo.martinez.sanchez@hotmail.com</b>
+                    </a>
+                </p>
+                <p>
+                    Si est&aacute;s buscando un desarrollador, tienes alguna pregunta o solamente quieres contactar conmigo,
+                    sientete libre de mandarme un correo o contactarme por instagram.
+                </p>
+            </div>
+        </section>
+    </section>
+    <footer>
+        <p>Copyright © 2020, Eduardo Mart&iacute;nez</p> 
+    </footer>
 </x-master>
