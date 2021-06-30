83af3626c177eb8cadf0dd4cae3d7b7cf6074f3d
Eduardo Martinez <eduardomartinez@neubox.net>
2021-06-30 13:29:23 -0500

    Adding GTM

--- resources/views/components/master.blade.php
+++ resources/views/components/master.blade.php
@@ -38,6 +38,14 @@
             gtag('config', "{{ env('GTAG') }}");
         </script>
 
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-5KXDC53');</script>
+        <!-- End Google Tag Manager -->
+
         <!-- Hotjar Tracking Code for https://lalocodes.com/ -->
         <script>
             (function(h,o,t,j,a,r){
@@ -55,7 +63,13 @@
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5KXDC53"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
+
         {{ $slot }}
+        
         <script src="{{ asset('js/app.js') }}"></script>
         <script async defer src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
     </body>
