a2714e33a99e8ff54e3ee07faa79ebc0dfc4d759
Martinez, Eduardo <eduardo.y.martinez@oracle.com>
2022-08-11 23:14:41 -0500

    Updating data of the page as well as the portfolio

--- resources/lang/en/text.php
+++ resources/lang/en/text.php
@@ -7,14 +7,14 @@ return [
     'hi' => 'Hi there,',
     'iAm' => "I'm Eduardo Mart&iacute;nez,",
     'passionateDeveloper' => 'A Passionate Web Developer',
-    'aboutFirst' => "I am 26 years old, at the age of 23 I graduated as an IT Engineer, as well as I am a web developer from Morelia, Michoac&aacute;n, Mex.",
-    'aboutSecond' => 'For almost 5 years I have worked in different positions such as: Systems Director, Tech Lead and Backend Developer.
+    'aboutFirst' => "I am 28 years old, at the age of 23 I graduated as an IT Engineer, as well as I am a web developer from Morelia, Michoac&aacute;n, Mex.",
+    'aboutSecond' => 'For almost 6 years I have worked in different positions such as: Systems Director, Tech Lead and Backend Developer.
     This has given me great knowledge and experience in managing and delivering projects, estimating times, developing high quality software, using different technologies, among other things.',
     'mySkills' => 'My skills',
     'aboutSkills' => 'During my working life I have developed several skills within the field of technology, 
     including the use of different programming languages, server management, frontend development, version control, among others.',
     'seeMore' => 'See more >',
-    'aboutTweety' => 'Tweety is a Laracast course project, in which I developed my skills on the Laravel framework.',
+    'aboutHGComercial' => 'HG Comercial is a real project for the company HG Comercial. This project was made in Wordpress with the Elementor plugin.',
     'aboutRestaurante' => 'It is a project of a landing page, making use of my knowledge acquired in CSS and SASS.',
     'aboutStartup' => 'It is a personal project that I built to practice my knowlegde about Bootstrap, CSS and SASS.',
     'contactText' => 'If you are looking for a developer, have any questions or just want to contact me, feel free to send me an email or contact me through my social media.',
